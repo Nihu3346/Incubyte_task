64611a59d23be93c7e7f7295564086bdb2fe8f7e
Nihar <18bce116@nirmauni.ac.in>
2021-08-25 10:47:28 +0530

    Testcase 2 commit

--- src/main/java/Caclulator/Calculator.java
+++ src/main/java/Caclulator/Calculator.java
@@ -1,7 +1,20 @@
 package Caclulator;
 
 public class Calculator {
+
+    //The method can take up to two numbers, separated by commas, and will return their sum.
+
     public static int addittion(String s) {
-        return 0;
+        if(s.equals(""))
+        {
+            //If string is empty return 0;
+            return 0;
+        }
+        else
+        {
+            //If string contains only number then return that number only
+            return Integer.parseInt(s);
+        }
+
     }
 }
--- src/main/java/Caclulator/CalculatorTest.java
+++ src/main/java/Caclulator/CalculatorTest.java
@@ -3,9 +3,21 @@ package Caclulator;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+/*
+Testcase 2: The method can take up to two numbers, separated by commas, and will return their sum.
+*/
+
 public class CalculatorTest {
     @Test
+    //for empty strings like ""
     public void TestCase_1_MustReturnZeroOnEmptyString(){
         assertEquals(0, Calculator.addittion(""));
+    }
+
+    @Test
+    //for strings which contains only number like "1"
+    public void TestCase_2_SimplyReturnNumber()
+    {
+        assertEquals(1,Calculator.addittion("1"));
     }
 }
